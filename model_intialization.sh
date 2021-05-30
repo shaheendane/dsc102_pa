1932baadcf2b549a3599afcc0c65cfcce5c5c066
shaheendane <62901626+shaheendane@users.noreply.github.com>
2021-05-30 15:31:27 -0700

    Update model_intialization.sh

--- model_intialization.sh
+++ model_intialization.sh
@@ -1,4 +1,8 @@
 sudo apt update
 sudo apt install awscli -y
 sudo apt install python3-pip -y
-
+pip install pandas
+pip install pyarrow
+pip install fsspec
+pip install s3fs
+aws s3 cp s3://ds102-teamaqua-scratch/model.py model.py
